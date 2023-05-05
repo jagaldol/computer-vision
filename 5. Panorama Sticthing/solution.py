69cc36afbbf2334135f9a3067b7f2928605c1e1d
jagaldol <yoy06056@pusan.ac.kr>
2023-05-05 17:42:14 +0900

    Rename: Separate RANSAC and Panorama

--- 5. Panorama Sticthing/solution.py
+++ 5. Panorama Sticthing/solution.py
@@ -95,6 +95,74 @@ def FindBestMatches(descriptors1, descriptors2, threshold):
     return matched_pairs
 
 
+def KeypointProjection(xy_points, h):
+    """
+    This function projects a list of points in the source image to the
+    reference image using a homography matrix `h`.
+    Inputs:
+        xy_points: numpy array, (num_points, 2)
+        h: numpy array, (3, 3), the homography matrix
+    Output:
+        xy_points_out: numpy array, (num_points, 2), input points in
+        the reference frame.
+    """
+    assert isinstance(xy_points, np.ndarray)
+    assert isinstance(h, np.ndarray)
+    assert xy_points.shape[1] == 2
+    assert h.shape == (3, 3)
+
+    # START
+    # Make homogeneous
+    xy_points_hg = np.append(xy_points, np.ones((xy_points.shape[0], 1)), axis=1)
+    # Apply h matrix
+    xy_points_proj = h.dot(xy_points_hg.T).T
+    # Avoid Divide By Zero
+    is_zero_points = xy_points_proj[:, 2] == 0
+    xy_points_proj[is_zero_points, 2] = 1e-10
+
+    # Return to Regular Coordinate
+    xy_points_out = xy_points_proj[:, :2] / xy_points_proj[:, 2:]
+
+    # END
+    return xy_points_out
+
+def RANSACHomography(xy_src, xy_ref, num_iter, tol):
+    """
+    Given matches of keyponit xy coordinates, perform RANSAC to obtain
+    the homography matrix. At each iteration, this function randomly
+    choose 4 matches from xy_src and xy_ref.  Compute the homography matrix
+    using the 4 matches.  Project all source "xy_src" keypoints to the
+    reference image.  Check how many projected keyponits are within a `tol`
+    radius to the coresponding xy_ref points (a.k.a. inliers).  During the
+    iterations, you should keep track of the iteration that yields the largest
+    inlier set. After the iterations, you should use the biggest inlier set to
+    compute the final homography matrix.
+    Inputs:
+        xy_src: a numpy array of xy coordinates, (num_matches, 2)
+        xy_ref: a numpy array of xy coordinates, (num_matches, 2)
+        num_iter: number of RANSAC iterations.
+        tol: float
+    Outputs:
+        h: The final homography matrix.
+    """
+    assert isinstance(xy_src, np.ndarray)
+    assert isinstance(xy_ref, np.ndarray)
+    assert xy_src.shape == xy_ref.shape
+    assert xy_src.shape[1] == 2
+    assert isinstance(num_iter, int)
+    assert isinstance(tol, (int, float))
+    tol = tol*1.0
+
+    # START
+
+
+
+    # END
+    assert isinstance(h, np.ndarray)
+    assert h.shape == (3, 3)
+    return h
+
+
 def FindBestMatchesRANSAC(
         keypoints1, keypoints2,
         descriptors1, descriptors2, threshold,
